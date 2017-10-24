1fdf9701b8e658ef320f44b7d3818fae02ab3ab3
gcnew <gcnew@abv.bg>
2017-10-24 16:24:25 +0300

    Implemented Leftist tree

---
+++ src/persistent/leftist_heap.ts
@@ -0,0 +1,105 @@
+
+import { OrdComparator } from '../ordering'
+
+export {
+    LeftistHeap, LeftistTree,
+
+    mkHeap, singleton, isEmpty, peek, pop, push, heapify
+}
+
+
+/* Types */
+
+// Height balanced leftist tree, the rank is the height of the tree.
+interface LeftistTree<T> {
+    item: T,
+    rank: number,
+    left: LeftistTree<T> | undefined,
+    right: LeftistTree<T> | undefined
+}
+
+interface LeftistHeap<T> {
+    comparator: OrdComparator<T>,
+    tree: LeftistTree<T> | undefined
+}
+
+
+/* API Functions */
+
+function isEmpty<T>(heap: LeftistHeap<T>): boolean {
+    return !heap.tree;
+}
+
+function peek<T>(heap: LeftistHeap<T>): T | undefined {
+    return heap.tree && heap.tree.item;
+}
+
+function pop<T>(heap: LeftistHeap<T>): [T, LeftistHeap<T>] | undefined {
+    return heap.tree && [
+        heap.tree.item,
+        mkHeap2(merge(heap.tree.left, heap.tree.right, heap.comparator), heap.comparator)
+    ];
+}
+
+function push<T>(item: T, heap: LeftistHeap<T>) {
+    return mkHeap2(
+        merge(mkTree(item, 1, undefined, undefined), heap.tree, heap.comparator),
+        heap.comparator
+    );
+}
+
+function heapify<T>(items: T[], comparator: OrdComparator<T>): LeftistHeap<T> {
+    return items.reduce(
+        (heap, x) => push(x, heap),
+        mkHeap(comparator)
+    );
+}
+
+
+/* Private Implementation Functions */
+
+function merge<T>(
+    tree1: LeftistTree<T> | undefined,
+    tree2: LeftistTree<T> | undefined,
+    cmp: OrdComparator<T>
+): LeftistTree<T> | undefined {
+    if (!tree1) {
+        return tree2;
+    }
+
+    if (!tree2) {
+        return tree1;
+    }
+
+    // favour left side
+    return cmp(tree1.item, tree2.item) !== 'GT'
+        ? join(tree1.item, tree1.left, merge(tree1.right, tree2, cmp))
+        : join(tree2.item, tree2.left, merge(tree1, tree2.right, cmp));
+}
+
+function join<T>(item: T, tree1: LeftistTree<T> | undefined, tree2: LeftistTree<T> | undefined) {
+    const rank1 = tree1 ? tree1.rank : 0;
+    const rank2 = tree2 ? tree2.rank : 0;
+
+    return (rank1 >= rank2)
+        ? mkTree(item, rank2 + 1, tree1, tree2)
+        : mkTree(item, rank1 + 1, tree2, tree1);
+}
+
+/* Constructors */
+
+function mkHeap<T>(comparator: OrdComparator<T>): LeftistHeap<T> {
+    return { comparator, tree: undefined };
+}
+
+function singleton<T>(item: T, comparator: OrdComparator<T>): LeftistHeap<T> {
+    return mkHeap2(mkTree(item, 1, undefined, undefined), comparator);
+}
+
+function mkHeap2<T>(tree: LeftistTree<T> | undefined, comparator: OrdComparator<T>): LeftistHeap<T> {
+    return { comparator, tree };
+}
+
+function mkTree<T>(item: T, rank: number, left: LeftistTree<T> | undefined, right: LeftistTree<T> | undefined): LeftistTree<T> {
+    return { item, rank, left, right };
+}
---
+++ tests/leftist_heap_tests.ts
@@ -0,0 +1,121 @@
+
+import { test } from 'pietr'
+import { assert } from 'chai'
+
+import { toComparator, invert } from '../src/ordering'
+import { LeftistHeap, mkHeap, singleton, isEmpty, push, pop, heapify } from '../src/persistent/leftist_heap'
+
+const numComp = toComparator(numOrdCmp);
+
+function numOrdCmp(x: number, y: number) {
+    return x < y ? 'LT' :
+           x > y ? 'GT' : 'EQ';
+}
+
+function heapSort(heap: LeftistHeap<number>) {
+    let item;
+    const retval = [];
+
+    while (!isEmpty(heap)) {
+        [ item, heap ] = pop(heap)!;
+        retval.push(item);
+    }
+
+    return retval;
+}
+
+function mkRandomArray(length: number) {
+    const retval = [];
+
+    for (let i = length; i > 0; --i) {
+        retval.push((Math.random() * 1000) | 0);
+    }
+
+    return retval;
+}
+
+test('Heapify', () => {
+    assert.deepEqual(heapSort(heapify([], numOrdCmp)), []);
+
+    for (let i = 0; i < 4; ++i) {
+        const arr = mkRandomArray(10 ** i);
+        const sorted = arr.slice().sort(numComp);
+
+        const heap = heapify(arr, numOrdCmp);
+        assert.deepEqual(heapSort(heap), sorted);
+    }
+});
+
+test('Push / Pop', () => {
+    for (let i = 0; i < 4; ++i) {
+        const arr = mkRandomArray(10 ** i);
+        const sorted = arr.slice().sort(numComp);
+
+        let heap = singleton(arr[0], numOrdCmp);
+        for (let k = 1; k < arr.length; ++k) {
+            heap = push(arr[k], heap);
+        }
+
+        let item;
+        while (sorted.length) {
+            [ item, heap ] = pop(heap)!;
+            assert.equal(item, sorted.shift());
+        }
+
+        assert.isTrue(isEmpty(heap));
+    }
+});
+
+test('Push / Pop - max heap', () => {
+    const ordCmp = invert(numOrdCmp);
+    const numCmp = (x: number, y: number) => numComp(x, y) * -1;
+
+    for (let i = 0; i < 4; ++i) {
+        const arr = mkRandomArray(10 ** i);
+        const sorted = arr.slice().sort(numCmp);
+
+        let heap = mkHeap(ordCmp);
+        for (let k = 0; k < arr.length; ++k) {
+            heap = push(arr[k], heap);
+        }
+
+        let item;
+        while (sorted.length) {
+            [ item, heap ] = pop(heap)!;
+            assert.equal(item, sorted.shift());
+        }
+
+        assert.isTrue(isEmpty(heap));
+    }
+});
+
+test('Persistence - push', () => {
+    const heaps = [ mkHeap(numOrdCmp) ];
+    const expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    for (let i = 0; i < expected.length; ++i) {
+        const last = heaps[heaps.length - 1];
+        heaps.push(push(expected[i], last));
+    }
+
+    for (let i = 0; i < heaps.length; ++i) {
+        const arr = heapSort(heaps[i]);
+        assert.deepEqual(arr, expected.slice(0, i));
+    }
+});
+
+test('Persistence - pop', () => {
+    const expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const heaps = [ heapify(expected, numOrdCmp) ];
+
+
+    for (let i = 0; i < expected.length; ++i) {
+        const last = heaps[heaps.length - 1];
+        heaps.push(pop(last)![1]);
+    }
+
+    for (let i = 0; i < heaps.length; ++i) {
+        const arr = heapSort(heaps[i]);
+        assert.deepEqual(arr, expected.slice(i));
+    }
+});
